268049cdd11f3c3576993b92bea3322d42b6dff0
azdragon2 <azdragon2@hotmail.com>
2020-09-29 23:29:38 -0700

    intial commit, wip

---
+++ src/test/java/com/valorogue/integrator/IntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorogue.integrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
